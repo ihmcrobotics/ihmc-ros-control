573998cff014b46b3e06ab82c93530ced93dee52
Jesper Smith <jsmith@ihmc.us>
2015-09-24 15:26:59 -0500

    Magic JVM starter

--- include/jvmLauncher/launcher.h
+++ include/jvmLauncher/launcher.h
@@ -1,6 +1,24 @@
 #ifndef LAUNCHER_H
 #define LAUNCHER_H
+#include <jni.h>
+#include <string>
 
+class Launcher
+{
+private:
+    JavaVM *jvm;
+    JNIEnv *env;
+    JavaVMInitArgs vmArguments;
+    void displayJNIError(std::string prefix, int error);
 
+public:
+    Launcher(std::string vmArguments);
+    bool startVM();
+    bool stopVM();
+
+    bool callStaticVoidMethod(std::string className, std::string method);
+
+    ~Launcher();
+};
 
 #endif // LAUNCHER_H
