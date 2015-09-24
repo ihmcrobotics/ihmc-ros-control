a4d91f6cb3747f684e9365dedfe1f5ff1736853b
Jesper Smith <jsmith@ihmc.us>
2015-09-24 11:59:13 -0500

    Added build files

---
+++ csrc/demo.cpp
@@ -0,0 +1,8 @@
+
+#include <jvmLauncher/launcher.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "HELLO WORLD" << std::endl;
+}
---
+++ include/jvmLauncher/launcher.h
@@ -0,0 +1,4 @@
+#ifndef LAUNCHER_H
+#define LAUNCHER_H
+
+#endif // LAUNCHER_H
